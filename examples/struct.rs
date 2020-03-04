9b42446bf5bd566470f73d0a71ee7ba87578f3c4
David Rivas <jdr@rivas-yee.com>
2020-03-03 23:05:40 -0800

    reactor.

--- examples/struct.rs
+++ examples/struct.rs
@@ -11,10 +11,11 @@ use chrono::Local;
 use clap::AppSettings;
 use linefeed::{Interface, ReadResult};
 use std::collections::HashMap;
+use std::env;
 use std::sync::{mpsc, Arc};
 use std::thread;
 use std::time::Duration;
-use std::{error, fmt, io};
+use std::{error, fmt};
 use structopt::StructOpt;
 
 fn main() -> Result<()> {
@@ -39,14 +40,162 @@ fn main() -> Result<()> {
     }
 
     // First time through.
-    let opt = AppCmds::from_args();
-    // eprintln!("Options are: {:?}", opt);
-    match parse_exec(RootCommands::App(opt)).unwrap() {
-        ParseResult::Interactive => readloop(),
-        _ => Ok(()),
-    };
+    match AppCmds::from_iter_safe(&env::args().collect::<Vec<String>>()[1..]) {
+        Ok(opt) => {
+            parse_app(opt)?;
+            ()
+        }
+        Err(e) => eprintln!("{}", e),
+    }
 
     Ok(())
+}
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "cli", version = "0.0.1", setting(AppSettings::NoBinaryName))]
+struct ICmds {
+    /// File name for configuration.
+    #[structopt(short = "c", long = "config", default_value = "cli.yaml")]
+    config: String,
+
+    #[structopt(subcommand)]
+    subcmd: InteractiveCommands,
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "cli", version = "0.0.1", setting(AppSettings::NoBinaryName))]
+struct AppCmds {
+    /// File name for configuration.
+    #[structopt(short = "c", long = "config", default_value = "cli.yaml")]
+    config: String,
+
+    #[structopt(subcommand)]
+    subcmd: RootSubcommand,
+}
+
+#[derive(StructOpt, Debug)]
+enum RootSubcommand {
+    Interactive,
+    #[structopt(flatten)]
+    InteractiveSubCommand(InteractiveCommands),
+}
+
+#[derive(StructOpt, Debug)]
+enum InteractiveCommands {
+    /// End the program.
+    #[structopt(name = "quit")]
+    Quit,
+
+    /// Make an http call.
+    HTTP(HTTPCmd),
+}
+
+#[derive(StructOpt, Debug)]
+struct HTTPCmd {
+    #[structopt(subcommand)]
+    subcmd: HTTPVerb,
+}
+
+#[derive(StructOpt, Debug)]
+enum HTTPVerb {
+    /// Make an http get call.
+    Get(HTTPArg),
+    /// Make an http put call.
+    Put(HTTPArg),
+}
+
+#[derive(StructOpt, Debug)]
+struct HTTPArg {
+    uri: String,
+    content: Vec<String>,
+    #[structopt(skip)]
+    method: attohttpc::Method,
+}
+
+fn parse_app(opt: AppCmds) -> Result<ParseResult> {
+    match opt.subcmd {
+        RootSubcommand::Interactive => {
+            readloop()?;
+            Ok(ParseResult::Exit)
+        }
+        RootSubcommand::InteractiveSubCommand(c) => parse_interactive(c),
+    }
+}
+
+fn parse_interactive(cmd: InteractiveCommands) -> Result<ParseResult> {
+    match cmd {
+        InteractiveCommands::HTTP(v) => {
+            let mut args: HTTPArg;
+            match v.subcmd {
+                HTTPVerb::Get(a) => {
+                    args = a;
+                    args.method = attohttpc::Method::GET;
+                }
+                HTTPVerb::Put(a) => {
+                    args = a;
+                    args.method = attohttpc::Method::PUT;
+                }
+            }
+
+            if args.content.len() > 0 {
+                println!("{} {} <{}>", args.method, args.uri, args.content.join(" "))
+            } else {
+                println!("{} {}", args.method, args.uri)
+            }
+            let resp = attohttpc::RequestBuilder::try_new(args.method, &args.uri)?.send()?;
+            println!("{:?}", resp);
+            Ok(ParseResult::Complete)
+        }
+        InteractiveCommands::Quit => Ok(ParseResult::Exit),
+    }
+}
+
+type Result<T> = std::result::Result<T, ParseError>;
+
+enum ParseResult {
+    Complete,
+    Exit,
+}
+
+#[derive(Debug)]
+enum ParseError {
+    Clap(clap::Error),
+    HTTP(attohttpc::Error),
+}
+
+impl fmt::Display for ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // write!(f, "{:?}", self)
+        match self {
+            ParseError::Clap(e) => match e.kind {
+                clap::ErrorKind::VersionDisplayed => write!(f, ""),
+                clap::ErrorKind::HelpDisplayed => write!(f, "{}", e.message),
+                _ => write!(f, "Parse error => {}", e),
+            },
+            ParseError::HTTP(e) => write!(f, "{}", e),
+        }
+    }
+}
+
+impl error::Error for ParseError {
+    fn cause(&self) -> Option<&dyn error::Error> {
+        None
+    }
+
+    fn description(&self) -> &str {
+        "parse error!"
+    }
+}
+
+impl From<clap::Error> for ParseError {
+    fn from(err: clap::Error) -> ParseError {
+        ParseError::Clap(err)
+    }
+}
+impl From<attohttpc::Error> for ParseError {
+    fn from(err: attohttpc::Error) -> Self {
+        ParseError::HTTP(err)
+    }
 }
 
 fn readloop() -> Result<()> {
@@ -66,8 +215,7 @@ fn readloop() -> Result<()> {
             Ok(Some(ReadResult::Input(line))) => {
                 let words: Vec<&str> = line.split_whitespace().collect();
                 match ICmds::from_iter_safe(words) {
-                    Ok(opt) => match parse_exec(RootCommands::Interactive(opt)) {
-                        Ok(ParseResult::Interactive) => continue, // should not get here.
+                    Ok(opt) => match parse_interactive(opt.subcmd) {
                         Ok(ParseResult::Complete) => continue,
                         Ok(ParseResult::Exit) => break,
                         Err(e) => eprintln!("{}", e),
@@ -125,159 +273,4 @@ fn prompt_start_up(tx: mpsc::Sender<PromptUpdate>) {
             i = i + 1;
         }
     });
-}
-
-#[derive(Debug)]
-enum RootCommands {
-    App(AppCmds),
-    Interactive(ICmds),
-}
-
-#[derive(StructOpt, Debug)]
-#[structopt(name = "cli", version = "0.0.1", setting(AppSettings::NoBinaryName))]
-struct ICmds {
-    /// File name for configuration.
-    #[structopt(short = "c", long = "config", default_value = "cli.yaml")]
-    config: String,
-
-    #[structopt(subcommand)]
-    subcmd: InteractiveCommands,
-}
-
-#[derive(Debug, StructOpt)]
-#[structopt(name = "cli-a", version = "0.0.1", setting(AppSettings::NoBinaryName))]
-struct AppCmds {
-    /// File name for configuration.
-    #[structopt(short = "c", long = "config", default_value = "cli.yaml")]
-    config: String,
-
-    #[structopt(subcommand)]
-    subcmd: RootSubcommand,
-}
-
-#[derive(StructOpt, Debug)]
-enum RootSubcommand {
-    Interactive,
-    #[structopt(flatten)]
-    InteractiveSubCommand(InteractiveCommands),
-}
-
-#[derive(StructOpt, Debug)]
-enum InteractiveCommands {
-    /// End the program.
-    #[structopt(name = "quit")]
-    Quit,
-
-    /// Make an http call.
-    HTTP(HTTPCmd),
-}
-
-#[derive(StructOpt, Debug)]
-struct HTTPCmd {
-    #[structopt(subcommand)]
-    subcmd: HTTPVerb,
-}
-
-#[derive(StructOpt, Debug)]
-enum HTTPVerb {
-    /// Make an http get call.
-    Get(HTTPArg),
-    /// Make an http put call.
-    Put(HTTPArg),
-}
-
-#[derive(StructOpt, Debug)]
-struct HTTPArg {
-    uri: String,
-    content: Vec<String>,
-    #[structopt(skip)]
-    method: attohttpc::Method,
-}
-
-fn parse_exec(opt: RootCommands) -> Result<ParseResult> {
-    match opt {
-        RootCommands::Interactive(c) => parse_interactive(c),
-        RootCommands::App(c) => parse_app(c),
-    }
-}
-
-fn parse_app(opt: AppCmds) -> Result<ParseResult> {
-    Ok(ParseResult::Interactive)
-}
-
-fn parse_interactive(opt: ICmds) -> Result<ParseResult> {
-    match opt.subcmd {
-        InteractiveCommands::HTTP(v) => {
-            let mut args: HTTPArg;
-            match v.subcmd {
-                HTTPVerb::Get(a) => {
-                    args = a;
-                    args.method = attohttpc::Method::GET;
-                }
-                HTTPVerb::Put(a) => {
-                    args = a;
-                    args.method = attohttpc::Method::PUT;
-                }
-            }
-
-            if args.content.len() > 0 {
-                println!("{} {} <{}>", args.method, args.uri, args.content.join(" "))
-            } else {
-                println!("{} {}", args.method, args.uri)
-            }
-            let resp = attohttpc::RequestBuilder::try_new(args.method, &args.uri)?.send()?;
-            println!("{:?}", resp);
-            Ok(ParseResult::Complete)
-        }
-        InteractiveCommands::Quit => Ok(ParseResult::Exit),
-    }
-}
-
-type Result<T> = std::result::Result<T, ParseError>;
-
-enum ParseResult {
-    Interactive,
-    Complete,
-    Exit,
-}
-
-#[derive(Debug)]
-enum ParseError {
-    Clap(clap::Error),
-    HTTP(attohttpc::Error),
-}
-
-impl fmt::Display for ParseError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        // write!(f, "{:?}", self)
-        match self {
-            ParseError::Clap(e) => match e.kind {
-                clap::ErrorKind::VersionDisplayed => write!(f, ""),
-                clap::ErrorKind::HelpDisplayed => write!(f, "{}", e.message),
-                _ => write!(f, "Parse error => {}", e),
-            },
-            ParseError::HTTP(e) => write!(f, "{}", e),
-        }
-    }
-}
-
-impl error::Error for ParseError {
-    fn cause(&self) -> Option<&dyn error::Error> {
-        None
-    }
-
-    fn description(&self) -> &str {
-        "parse error!"
-    }
-}
-
-impl From<clap::Error> for ParseError {
-    fn from(err: clap::Error) -> ParseError {
-        ParseError::Clap(err)
-    }
-}
-impl From<attohttpc::Error> for ParseError {
-    fn from(err: attohttpc::Error) -> Self {
-        ParseError::HTTP(err)
-    }
 }
